f5a5f05874a903d9a0c9275c2b55db264b958251
leshay-adams <leshayadams96@gmail.com>
2025-03-16 15:25:13 +0200

    Create 50 new randomized users

--- src/api/mockApi.ts
+++ src/api/mockApi.ts
@@ -11,3 +11,19 @@ export interface Role {
   id: string
   name: string
 }
+
+const roles: Readonly<Role[]> = [
+  {id: 'admin', name: 'Admin'},
+  {id: 'manager', 'name': 'Manager'},
+  {id: 'viewer', name: 'Viewer'}
+]
+const pastSixMonths = Date.now() - 180 * 24 * 60 * 60 * 1000;
+
+let users: User[] = [...Array(50)].map((_, i) => ({
+  id: (i + 1).toString(),
+  name: `User ${i + 1}`,
+  email: `alias${i + 1}@metavoid.com`,
+  role: roles[i % 3].id,
+  status: i % 2 ? 'inactive' : 'active',
+  dateJoined: new Date(pastSixMonths + Math.random() * (Date.now() - pastSixMonths)).toISOString()
+}))
