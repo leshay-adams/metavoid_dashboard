a48324510576c03646f62f68885ec759f701a898
leshay-adams <leshayadams96@gmail.com>
2025-03-16 15:46:09 +0200

    get users, get single user, add delay simulation

--- src/api/mockApi.ts
+++ src/api/mockApi.ts
@@ -27,3 +27,37 @@ let users: User[] = [...Array(50)].map((_, i) => ({
   status: i % 2 ? 'inactive' : 'active',
   dateJoined: new Date(pastSixMonths + Math.random() * (Date.now() - pastSixMonths)).toISOString()
 }))
+
+const simDelay = (min = 300, max = 800) => (
+  new Promise(res => setTimeout(res, Math.random() * (max - min) + min))
+)
+
+export const mockApi = {
+
+  async getUser(id: string) {
+    await simDelay()
+    return users.find(i => i.id === id) || Promise.reject('Error: No User found')
+  },
+
+  async getUsers({ page = 1, limit = 10, filter = {}, sort = {} }: GetUsersParams = {}) {
+    await simDelay();
+    let data = users.filter(i =>
+      (!filter.name || i.name.toLowerCase().includes(filter.name.toLowerCase())) &&
+      (!filter.role || i.role === filter.role) &&
+      (!filter.status || i.status === filter.status)
+    );
+
+    if (sort.field) {
+      data.sort((a, b) =>
+        sort.field === 'name' 
+          ? sort.order === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name)
+          : sort.order === 'asc' 
+          ? new Date(a.dateJoined).getTime() - new Date(b.dateJoined).getTime()
+          : new Date(b.dateJoined).getTime() - new Date(a.dateJoined).getTime()
+      );
+    }
+
+    return { data: data.slice((page - 1) * limit, page * limit), total: data.length, page, limit };
+  },
+
+}
