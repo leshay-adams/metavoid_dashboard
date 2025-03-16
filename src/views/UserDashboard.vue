0dc22e4bc662528150191c95ec6b3eddc7141350
leshay-adams <leshayadams96@gmail.com>
2025-03-16 14:33:33 +0200

    add routing, component structure, UserInfo

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-
+import UsersList from '../components/UsersList.vue'
 </script>
 
 <template>
   <div>List of Users</div>
+  <UsersList />
 </template>
---
+++ src/views/UserInfo.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import UsersList from '../components/UsersList.vue'
+</script>
+
+<template>
+  <div>Details of a Users</div>
+</template>
