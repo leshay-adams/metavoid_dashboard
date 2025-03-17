6404da98a6a75a0a0959d74092daed4cd79dc4ba
leshay-adams <leshayadams96@gmail.com>
2025-03-17 09:25:12 +0200

    Basic pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+  import { computed, defineProps, defineEmits } from 'vue'
+
+  const props = defineProps({
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    totalPages: {
+      type: Number,
+      required: true
+    }
+  })
+
+  const emit = defineEmits(['pagechanged'])
+
+  const isOnFirstPage = computed(() => props.currentPage === 1)
+  const isOnLastPage = computed(() => props.currentPage === props.totalPages)
+
+  const pages = computed(() => {
+    const visiblePages: { name: number; disabled: boolean }[] = []
+    for (let i = 1; i <= props.totalPages; i++) {
+      visiblePages.push({ name: i, isDisabled: i === props.currentPage })
+    }
+    return visiblePages
+  })
+
+</script>
+
+<template>
+  <button @click="" :disabled="isOnFirstPage" type="button">
+    1
+  </button>
+  <button @click="" :disabled="isOnFirstPage" type="button">
+    <<
+  </button>
+  <button @click="" :disabled="isOnFirstPage" type="button">
+    >>
+  </button>
+  <button @click="" :disabled="isOnFirstPage" type="button">
+    End
+  </button>
+</template>
